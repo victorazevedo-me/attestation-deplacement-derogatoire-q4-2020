191b89abac543b15b2bf40a428b39a21ef4be204
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-11-03 11:59:06 +0100

    Removed updates & date de sortie aléatoire

--- src/js/form.js
+++ src/js/form.js
@@ -7,160 +7,181 @@ import formData from '../form-data.json'
 import { $, appendTo, createElement } from './dom-utils'
 
 const createTitle = () => {
-  const h2 = createElement('h2', { className: 'titre-2', innerHTML: 'Remplissez en ligne votre déclaration numérique : ' })
-  const p = createElement('p', { className: 'msg-info', innerHTML: 'Tous les champs sont obligatoires.' })
-  return [h2, p]
+    const h2 = createElement('h2', {
+        className: 'titre-2',
+        innerHTML: 'Remplissez en ligne votre déclaration numérique : ',
+    })
+    const p = createElement('p', {
+        className: 'msg-info',
+        innerHTML: 'Tous les champs sont obligatoires.',
+    })
+    return [h2, p]
 }
 // createElement('div', { className: 'form-group' })
 
 const getCurrentTime = () => {
-  const date = new Date()
-  return date.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })
+    const nowdate = new Date()
+
+    //entre 15 et 40 minutes aleatoire
+    const randomMinute = Math.floor(Math.random() * 25) + 15
+    const spoofdate = new Date(nowdate.getTime() - 60000 * randomMinute)
+
+    return spoofdate.toLocaleTimeString('fr-FR', {
+        hour: '2-digit',
+        minute: '2-digit',
+    })
 }
 
 const createFormGroup = ({
-  autocomplete = false,
-  autofocus = false,
-  inputmode,
-  label,
-  max,
-  min,
-  maxlength,
-  minlength,
-  name,
-  pattern,
-  placeholder = '',
-  type = 'text',
-}) => {
-  const formGroup = createElement('div', { className: 'form-group' })
-  const labelAttrs = {
-    for: `field-${name}`,
-    id: `field-${name}-label`,
-    innerHTML: label,
-  }
-  const labelEl = createElement('label', labelAttrs)
-
-  const inputGroup = createElement('div', { className: 'input-group align-items-center' })
-  const inputAttrs = {
-    autocomplete,
-    autofocus,
-    className: 'form-control',
-    id: `field-${name}`,
+    autocomplete = false,
+    autofocus = false,
     inputmode,
-    min,
+    label,
     max,
-    minlength,
+    min,
     maxlength,
+    minlength,
     name,
     pattern,
-    placeholder,
-    required: true,
-    type,
-  }
+    placeholder = '',
+    type = 'text',
+}) => {
+    const formGroup = createElement('div', { className: 'form-group' })
+    const labelAttrs = {
+        for: `field-${name}`,
+        id: `field-${name}-label`,
+        innerHTML: label,
+    }
+    const labelEl = createElement('label', labelAttrs)
 
-  const input = createElement('input', inputAttrs)
+    const inputGroup = createElement('div', {
+        className: 'input-group align-items-center',
+    })
+    const inputAttrs = {
+        autocomplete,
+        autofocus,
+        className: 'form-control',
+        id: `field-${name}`,
+        inputmode,
+        min,
+        max,
+        minlength,
+        maxlength,
+        name,
+        pattern,
+        placeholder,
+        required: true,
+        type,
+    }
 
-  if (name === 'heuresortie') {
-    input.value = getCurrentTime()
-  }
+    const input = createElement('input', inputAttrs)
 
-  const validityAttrs = {
-    className: 'validity',
-  }
-  const validity = createElement('span', validityAttrs)
+    if (name === 'heuresortie') {
+        input.value = getCurrentTime()
+    }
 
-  const appendToFormGroup = appendTo(formGroup)
-  appendToFormGroup(labelEl)
-  appendToFormGroup(inputGroup)
+    const validityAttrs = {
+        className: 'validity',
+    }
+    const validity = createElement('span', validityAttrs)
 
-  const appendToInputGroup = appendTo(inputGroup)
-  appendToInputGroup(input)
-  appendToInputGroup(validity)
+    const appendToFormGroup = appendTo(formGroup)
+    appendToFormGroup(labelEl)
+    appendToFormGroup(inputGroup)
 
-  return formGroup
+    const appendToInputGroup = appendTo(inputGroup)
+    appendToInputGroup(input)
+    appendToInputGroup(validity)
+
+    return formGroup
 }
 
 const createReasonField = (reasonData) => {
-  const formReasonAttrs = { className: 'form-checkbox align-items-center' }
-  const formReason = createElement('div', formReasonAttrs)
-  const appendToReason = appendTo(formReason)
+    const formReasonAttrs = { className: 'form-checkbox align-items-center' }
+    const formReason = createElement('div', formReasonAttrs)
+    const appendToReason = appendTo(formReason)
 
-  const id = `checkbox-${reasonData.code}`
-  const inputReasonAttrs = {
-    className: 'form-check-input',
-    type: 'checkbox',
-    id,
-    name: 'field-reason',
-    value: reasonData.code,
-  }
-  const inputReason = createElement('input', inputReasonAttrs)
+    const id = `checkbox-${reasonData.code}`
+    const inputReasonAttrs = {
+        className: 'form-check-input',
+        type: 'checkbox',
+        id,
+        name: 'field-reason',
+        value: reasonData.code,
+    }
+    const inputReason = createElement('input', inputReasonAttrs)
 
-  const labelAttrs = { innerHTML: reasonData.label, className: 'form-checkbox-label', for: id }
-  const label = createElement('label', labelAttrs)
+    const labelAttrs = {
+        innerHTML: reasonData.label,
+        className: 'form-checkbox-label',
+        for: id,
+    }
+    const label = createElement('label', labelAttrs)
 
-  appendToReason([inputReason, label])
-  return formReason
+    appendToReason([inputReason, label])
+    return formReason
 }
 
 const createReasonFieldset = (reasonsData) => {
-  const fieldsetAttrs = {
-    id: 'reason-fieldset',
-    className: 'fieldset',
-  }
+    const fieldsetAttrs = {
+        id: 'reason-fieldset',
+        className: 'fieldset',
+    }
 
-  const fieldset = createElement('fieldset', fieldsetAttrs)
-  const appendToFieldset = appendTo(fieldset)
+    const fieldset = createElement('fieldset', fieldsetAttrs)
+    const appendToFieldset = appendTo(fieldset)
 
-  const legendAttrs = {
-    className: 'legend titre-3',
-    innerHTML: 'Choisissez un motif de déplacement',
-  }
-  const legend = createElement('legend', legendAttrs)
+    const legendAttrs = {
+        className: 'legend titre-3',
+        innerHTML: 'Choisissez un motif de déplacement',
+    }
+    const legend = createElement('legend', legendAttrs)
 
-  const textAlertAttrs = { className: 'msg-alert hidden', innerHTML: 'Veuillez choisir un motif' }
-  const textAlert = createElement('p', textAlertAttrs)
+    const textAlertAttrs = {
+        className: 'msg-alert hidden',
+        innerHTML: 'Veuillez choisir un motif',
+    }
+    const textAlert = createElement('p', textAlertAttrs)
 
-  const textSubscribeReasonAttrs = {
-    innerHTML: 'certifie que mon déplacement est lié au motif suivant (cocher la case) autorisé par le décret n°2020-1310 du 29 octobre 2020 prescrivant les mesures générales nécessaires pour faire face à l\'épidémie de Covid19 dans le cadre de l\'état d\'urgence sanitaire  <a class="footnote" href="#footnote1">[1]</a>&nbsp;:',
-  }
+    const textSubscribeReasonAttrs = {
+        innerHTML:
+            'certifie que mon déplacement est lié au motif suivant (cocher la case) autorisé par le décret n°2020-1310 du 29 octobre 2020 prescrivant les mesures générales nécessaires pour faire face à l\'épidémie de Covid19 dans le cadre de l\'état d\'urgence sanitaire  <a class="footnote" href="#footnote1">[1]</a>&nbsp;:',
+    }
 
-  const textSubscribeReason = createElement('p', textSubscribeReasonAttrs)
+    const textSubscribeReason = createElement('p', textSubscribeReasonAttrs)
 
-  const reasonsFields = reasonsData.items.map(createReasonField)
+    const reasonsFields = reasonsData.items.map(createReasonField)
 
-  appendToFieldset([legend, textAlert, textSubscribeReason, ...reasonsFields])
-  // Créer un form-checkbox par motif
-  return fieldset
+    appendToFieldset([legend, textAlert, textSubscribeReason, ...reasonsFields])
+    // Créer un form-checkbox par motif
+    return fieldset
 }
 
-export function createForm () {
-  const form = $('#form-profile')
-  // Évite de recréer le formulaire s'il est déjà créé par react-snap (ou un autre outil de prerender)
-  if (form.innerHTML !== '') {
-    return
-  }
+export function createForm() {
+    const form = $('#form-profile')
+    // Évite de recréer le formulaire s'il est déjà créé par react-snap (ou un autre outil de prerender)
+    if (form.innerHTML !== '') {
+        return
+    }
 
-  const appendToForm = appendTo(form)
+    const appendToForm = appendTo(form)
 
-  const formFirstPart = formData
-    .flat(1)
-    .filter(field => field.key !== 'reason')
-    .filter(field => !field.isHidden)
-    .map((field,
-      index) => {
-      const formGroup = createFormGroup({
-        autofocus: index === 0,
-        ...field,
-        name: field.key,
-      })
+    const formFirstPart = formData
+        .flat(1)
+        .filter((field) => field.key !== 'reason')
+        .filter((field) => !field.isHidden)
+        .map((field, index) => {
+            const formGroup = createFormGroup({
+                autofocus: index === 0,
+                ...field,
+                name: field.key,
+            })
 
-      return formGroup
-    })
+            return formGroup
+        })
 
-  const reasonsData = formData
-    .flat(1)
-    .find(field => field.key === 'reason')
+    const reasonsData = formData.flat(1).find((field) => field.key === 'reason')
 
-  const reasonFieldset = createReasonFieldset(reasonsData)
-  appendToForm([...createTitle(), ...formFirstPart, reasonFieldset])
+    const reasonFieldset = createReasonFieldset(reasonsData)
+    appendToForm([...createTitle(), ...formFirstPart, reasonFieldset])
 }
